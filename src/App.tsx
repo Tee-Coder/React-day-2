5039074a8a5b2f1d048485a9f15d507a9a3182a7
Toyin <oluwatoyinogundare@gmail.com>
2020-04-08 14:21:50 -0600

    new react 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Heading } from './Components/Heading';
+import Person from './Components/Person';
 import logo from './logo.svg';
 import './App.css';
+
+const myPersonFunction = (input: string): string => {
+  return 'Our Person is Important' + input;
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Heading />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,6 +25,7 @@ function App() {
         >
           Learn React
         </a>
+        <Person name = "Billy-Joe Bob" callbackMethod={myPersonFunction}/>
       </header>
     </div>
   );
