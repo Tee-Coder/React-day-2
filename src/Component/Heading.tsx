4985577d7b8d63b3d4da1fdc5566728aa710f28d
Toyin <oluwatoyinogundare@gmail.com>
2020-04-08 11:15:28 -0600

    new react 2

--- src/Component/Heading.tsx
+++ src/Component/Heading.tsx
@@ -1,2 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+class Heading extends React.Component{
+
+    render ()
+    {
+        return <h1>Our First Component</h1>
+    }
+}
